a92610fd757e68628f5fc0065d1cdc75fc614053
Derek Lee <derek.scott.lee13@gmail.com>
2023-01-17 01:13:11 -0800

    Support MiniMessage in broadcasts

--- src/main/java/xyz/holocons/mc/proxy/commands/BroadcastCommand.java
+++ src/main/java/xyz/holocons/mc/proxy/commands/BroadcastCommand.java
@@ -4,10 +4,11 @@ import com.velocitypowered.api.command.RawCommand;
 import com.velocitypowered.api.proxy.ConsoleCommandSource;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.audience.MessageType;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public final class BroadcastCommand implements RawCommand {
+
+    private static final String PREFIX = "<yellow>[<dark_red>Broadcast</dark_red>]</yellow> ";
 
     private final ProxyServer proxy;
 
@@ -17,12 +18,7 @@ public final class BroadcastCommand implements RawCommand {
 
     @Override
     public void execute(final Invocation invocation) {
-        final var message = Component.text()
-                .append(Component.text('[', NamedTextColor.YELLOW))
-                .append(Component.text("Broadcast", NamedTextColor.DARK_RED))
-                .append(Component.text("] ", NamedTextColor.YELLOW))
-                .append(Component.text(invocation.arguments(), NamedTextColor.DARK_GREEN))
-                .build();
+        final var message = MiniMessage.miniMessage().deserialize(PREFIX + invocation.arguments());
 
         proxy.getAllPlayers().forEach(player -> player.sendMessage(message, MessageType.SYSTEM));
     }
